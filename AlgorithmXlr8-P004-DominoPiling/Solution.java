5df099ca1043813b13cba6af95eab740281fea76
Atul Dahiya <87331579+atuldahiya3@users.noreply.github.com>
2026-08-21 00:21:10 +0530

    Solve Domino Piling

---
+++ AlgorithmXlr8-P004-DominoPiling/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println((m*n)/2);
+        return;
+        // Write your solution here.
+        // Find the maximum number of 1x2 dominoes that fit on an n x m board.
+    }
+}
